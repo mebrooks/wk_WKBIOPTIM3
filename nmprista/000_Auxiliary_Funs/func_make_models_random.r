07cbfdf5f8af3c4724fc9c825a9f43ff91948a50
nmprista <31890653+nmprista@users.noreply.github.com>
2019-05-25 22:40:18 +0200

    Update func_make_models_random.r
    - added repl (useful in parallel)

--- nmprista/000_Auxiliary_Funs/func_make_models_random.r
+++ nmprista/000_Auxiliary_Funs/func_make_models_random.r
@@ -1,4 +1,4 @@
-make_models_random<-function(y, model){				
+make_models_random<-function(y, model, repl = 1){				
 					
 					out_list<-list("weight-length"=NULL, "sex-ratio"=NULL, "L50"=NULL, "VBGF"=NULL)
 						
@@ -14,9 +14,9 @@ make_models_random<-function(y, model){
 							if(nrow(na.omit(x[c("indWt","lenCls")]))>2)
 									{
 									model_weight_length<-lm(log(x$indWt)~log(x$lenCls+variable_table[variable_table$variable=="lenCls","original_class_span"]/2), weights= x$samp_weight); 
-									out<-data.frame(n = nrow(na.omit(x[c("indWt","lenCls")])), log_a=coef(model_weight_length)[1], b=coef(model_weight_length)[2], r.squared=summary(model_weight_length)$r.squared)
+									out<-data.frame(repl = repl, n = nrow(na.omit(x[c("indWt","lenCls")])), log_a=coef(model_weight_length)[1], b=coef(model_weight_length)[2], r.squared=summary(model_weight_length)$r.squared)
 									} else {
-											out<-data.frame(n = nrow(na.omit(x[c("indWt","lenCls")])), log_a=NA, b=NA, r.squared=NA)
+											out<-data.frame(repl = repl, n = nrow(na.omit(x[c("indWt","lenCls")])), log_a=NA, b=NA, r.squared=NA)
 											}
 							}				
 
@@ -26,10 +26,10 @@ make_models_random<-function(y, model){
 							if(sum(!is.na(x$sex))>5)
 												{
 												a<-table(x$sex, useNA="al")
-												out<-data.frame(n_females = a["F"], n_males=a["M"], n_indeterm = sum(a)-a["F"]-a["M"], sex_ratio=a["F"]/a["M"]); 
+												out<-data.frame(repl = repl, n_females = a["F"], n_males=a["M"], n_indeterm = sum(a)-a["F"]-a["M"], sex_ratio=a["F"]/a["M"]); 
 												rownames(out)<-NULL
 												} else {
-														out<-data.frame(n_females = sum(x$sex=="F", na.rm=T), n_males=sum(x$sex=="M", na.rm=T), n_indeterm = sum(x$sex=="I", na.rm=T), sex_ratio=NA)
+														out<-data.frame(repl = repl, n_females = sum(x$sex=="F", na.rm=T), n_males=sum(x$sex=="M", na.rm=T), n_indeterm = sum(x$sex=="I", na.rm=T), sex_ratio=NA)
 														}
 							}
 					
@@ -41,9 +41,9 @@ make_models_random<-function(y, model){
 												#browser()
 												#x$mature<-as.numeric(as.character(x$mature))
 												model_maturation<-glm(mature~lenCls,data=x[c("mature","lenCls")],family=binomial,weights= x$samp_weight); 
-												out<-data.frame(n = nrow(na.omit(x[c("lenCls","mature")])), n_immature = sum(x$mature==0, na.rm=T), n_mature = sum(x$mature==1, na.rm=T), L50 = lrPerc(coef(model_maturation),0.5))
+												out<-data.frame(repl = repl, n = nrow(na.omit(x[c("lenCls","mature")])), n_immature = sum(x$mature==0, na.rm=T), n_mature = sum(x$mature==1, na.rm=T), L50 = lrPerc(coef(model_maturation),0.5))
 												} else {
-														out<-data.frame(n = nrow(na.omit(x[c("mature","lenCls")])), n_immature = sum(x$mature==0, na.rm=T), n_mature = sum(x$mature==1, na.rm=T), L50=NA)
+														out<-data.frame(repl = repl, n = nrow(na.omit(x[c("mature","lenCls")])), n_immature = sum(x$mature==0, na.rm=T), n_mature = sum(x$mature==1, na.rm=T), L50=NA)
 														}
 							}
 					## models: von Bertalanffy growth model
@@ -67,14 +67,14 @@ make_models_random<-function(y, model){
 									Linf<- summary(fitTypical)$coefficients[[1]]
 									K<- summary(fitTypical)$coefficients[[2]]
 									t0<- summary(fitTypical)$coefficients[[3]]
-									out<- data.frame(Linf, K, t0)
-									} else {out<- data.frame(Linf=NA, K=NA, t0=NA); 
+									out<- data.frame(repl = repl, Linf, K, t0)
+									} else {out<- data.frame(repl = repl, Linf=NA, K=NA, t0=NA); 
 										#print("Did not converge")
 										}
 								#print(paste("VBGM parameters estimated: ",out))
 								
 							  } else {
-								out<- data.frame(Linf=NA, K=NA, t0=NA)  ##message of not enough age groups
+								out<- data.frame(repl = repl, Linf=NA, K=NA, t0=NA)  ##message of not enough age groups
 								test <- nrow(na.omit(x[c("age","lenCls")]))
 								#print((unique(x$age)))
 								#print(paste("Not enough data to obtain the VBGF parameters: only",test,"indiv (min = 10 indiv)"))
